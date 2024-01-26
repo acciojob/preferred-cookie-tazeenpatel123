e28d1b3662e572c2a5993e132e54ed6fc61291f7
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2024-01-26 21:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+//your JS code here. If required.
+document.getElementById("myForm").addEventListener("submit",(e)=>{
+
+	e.preventDefault();
+	let fontsize = document.getElementById("fontsize").value;
+	let fontcolor = document.getElementById("fontcolor").value;
+
+	document.cookie = `fontsize=${fontsize};`
+	document.cookie = `fontcolor=${fontcolor};`
+	
+document.documentElement.style.setProperty("--fontsize", `${fontsize??16}px`);
+document.documentElement.style.setProperty("--fontcolor", `${fontcolor??"#000"}`);
+})
+
+let cookies = Object.fromEntries(
+	document.cookie.split("; ").
+	map((item)=>item.split("=")));
+
+document.getElementById("fontsize").value = cookies.fontsize??16;
+document.getElementById("fontcolor").value = cookies.fontcolor??"#000000";
+
+document.documentElement.style.setProperty("--fontsize", `${cookies.fontsize??16}px`);
+document.documentElement.style.setProperty("--fontcolor", `${cookies.fontcolor??"#000"}`);
